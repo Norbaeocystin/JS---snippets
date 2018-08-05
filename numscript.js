0a2efb94321d1df03952f6661f8b33b49e57da07
Norbaeocystin <baranrastislav@gmail.com>
2018-08-05 18:50:24 +0200

    Update numscript.js

--- numscript.js
+++ numscript.js
@@ -1,11 +1,28 @@
-function Reshape(array, reshape){ // change shape of flatten array
+function Reshape(array, reshape)// change flatten array to nested array
 /*
 Reshape([1,2,3,4],2)
 [[1,2],[3,4]]
 */
+{ 
   var arrayReshaped = []
   var i = 0;
-  while (i < array.length) {
+  while (i < array.length) 
+  {
     arrayReshaped.push(array.slice(i, i += reshape));
   }
-  return arrayReshaped}
+  return arrayReshaped;
+}
+
+function Flatten(array) // flatten nested array
+/*
+Flatten([[1,2],[3,4]])
+[1,2,3,4]
+*/
+{
+  var Flattened = [];
+  for (subarray in array)
+  { 
+    Flattened = Flattened.concat(array[subarray])
+  }
+return Flattened
+}
